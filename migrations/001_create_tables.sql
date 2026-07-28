01dd57527fc33b6351cdaaf32c65ac4e2aeaa2c3
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-07-28 11:13:58 +0530

    migration maded

---
+++ migrations/001_create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+);
+CREATE TABLE todos(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    title VARCHAR(255) NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT FALSE,
+);
